ff8bc7db587d3cce632a5716166fe1254bef4481
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-05-06 11:12:39 +0800

    build commit

--- app/page.jsx
+++ app/page.jsx
@@ -4,9 +4,6 @@ import TopAiring from "./pages/topairingpage"
 import RankingList from "./pages/rankpage";
 
 export default async function Home() {
-
-  
-
   const topAnimeAirRes = await fetch(`${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?filter=airing&limit=12`)
   const airAnime = await topAnimeAirRes.json();
   const topAirAnime = airAnime.data
