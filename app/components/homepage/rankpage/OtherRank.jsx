fe9458fc661e173ba5915b72e6de323acfa6cef9
anggspradivts <139469888+anggspradivts@users.noreply.github.com>
2024-05-08 14:37:10 +0800

    fix bugs

--- app/components/homepage/rankpage/OtherRank.jsx
+++ app/components/homepage/rankpage/OtherRank.jsx
@@ -36,7 +36,7 @@ const OtherRank = async ({ topPeople }) => {
   });
 
   const charCard =
-    topChar?.map((char, index) => {
+    topChar.map((char, index) => {
       return (
         <div key={index}>
           <Link href={char.url}>
